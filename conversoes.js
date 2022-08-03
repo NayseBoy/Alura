eb9c65b91f206295f19b660e1b098302edd08dec
kaique <kaiquejesus7567@gmail.com>
2022-08-03 20:35:47 -0300

    codigos Var e Conversoes

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// node conversoes.js
+
+// tipo de dado
+// booleanos
+
+//conversão inplicita
+const numero = 456;
+const numeroString = Number("456");
+
+//Number()
+//String()
+console.log(numero + numeroString);
+
+
+// conversao explicita
--- tipos primitivos/teste.js
+++ tipos primitivos/teste.js
@@ -1,4 +1,4 @@
-const cifrao = '\u0024'
+/*const cifrao = '\u0024'
 const aMaiusculo = '\u0041'
 const tique = '\u2705'
 const hiragana = '\u3041'
@@ -18,3 +18,10 @@ console.log(cidade === inputMinusculo); //
 
 const senha = "minhaSenha123"
 console.log(senha.length) // 13 caracteres
+*/
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
