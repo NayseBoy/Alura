1a1328ab6d705ab2a0398aafab60eef21bd402de
kaique <kaiquejesus7567@gmail.com>
2022-08-03 12:30:05 -0300

    exemplos da aula 1

---
+++ type-nunber.js
@@ -0,0 +1,24 @@
+// tipo nunber;
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2.5;
+
+const operaçaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operaçaoMatematica)
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5; 
+
+const novaOperaçao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperaçao);
+
+//NaN Not A Nunber (não é um numero)
+
+const alura = "Alura";1
+console.log(alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,15 @@
+//Tipo String
+
+const texto1 = "Olá Mundo";
+const texto2 ='Olá Mundo';
+const senha = "senhaaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citaçao = "meu nome é ";
+const meuNome = "kaique";
+
+// concatenação (+)
+
+console.log(citaçao + meuNome);
+
+//template string OU template literal
